a3caa3ba5071bdf57277893c81fd05db3a6e71ba
Anton Balykov <Balykov1987@gmail.com>
2023-03-27 13:22:04 +0400

    Update send.php

--- send.php
+++ send.php
@@ -2,9 +2,6 @@
 // Load WordPress
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
 
-//$upload_dir = wp_upload_dir('/wp-content/themes/shifters-pro/quiz/admin');
- 
-
 
 add_filter( 'wp_mail_content_type', 'get_content_type' );
 
@@ -19,10 +16,7 @@ function send_mail(){
 
 	global $wpdb;
 	$data = get_admin_mail();
-	//$user = '76';
-	// 'devantonovtag@yandex.ru'; 
 	$email = $data['admin_mail_address'] ?? '';
-///$attachment = array(WP_CONTENT_DIR . '/themes/shifters-pro/quiz/admin/' . $user . '.pdf');
 	wp_mail(
 		$email,
 		'New order',
